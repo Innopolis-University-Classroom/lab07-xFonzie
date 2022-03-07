7d3465c59ea018a29a00d8a23851d2783f5ca76e
Danil <danilkuchukov303@gmail.com>
2022-03-07 13:20:57 +0300

    ex1 and ex2 done

--- ex01.hpp
+++ ex01.hpp
@@ -2,7 +2,8 @@
 //change the below code
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
-void multiples (int& sum, int x, int n)
+template <class T>
+void multiples (T& sum, T x, int n)
 {
-
+    sum = 1 + (n * (n + 1) / 2) * x;
 }
--- ex02.hpp
+++ ex02.hpp
@@ -2,20 +2,21 @@
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
 
+template <class T>
 public class Calculator {
 private:
-    int num1, num2;
+    T num1, num2;
 
 public:
-    Calculator(int n1, int n2) {
+    Calculator(T n1, T n2) {
         num1 = n1;
         num2 = n2;
     }
 
-    int add() { return 0; /*your code here*/ }
-    int subtract() { /*your code here*/ }
-    int multiply() { /*your code here*/ }
-    int divide() { /*your code here*/ }
+    int add() { return num1 + num2; }
+    int subtract() { return num1 - num2; }
+    int multiply() { return num1 * num2; }
+    int divide() { return num1 / num2; }
 };
 
 
